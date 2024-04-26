27b91095d2df1a36cc1cf2ec3ae4c6ad554cf6cc
Timo Becker <3785547+timopb@users.noreply.github.com>
2024-04-26 18:11:28 +0200

    changed string concatination to format strings

--- app/prompt_generators/metharme.py
+++ app/prompt_generators/metharme.py
@@ -12,22 +12,22 @@ def build_metharme_prompt(conf, query,history,user_name):
     <|user|>How much is the fish?<|model|>
     """
 
-    prompt = "<|system|>" + conf.SYSTEM_MESSAGE
+    prompt = f"<|system|>{conf.SYSTEM_MESSAGE}"
 
     # Add warmup message to kick off chat into the right direction unless
     # we have a decent amount of chat history
     if hasattr(conf,'WARMUP') and (len(history) <= conf.HISTORY_COUNT):
-        prompt += "\n" + conf.WARMUP
+        prompt += f"\n{conf.WARMUP}"
 
-    prompt += "\n\nGenerate " + conf.BOT_NAME + "'s response to the following conversation. Enclose emotes in asterisks. Always stay in character.\n"
+    prompt += f"\n\nGenerate {conf.BOT_NAME}'s response to the following conversation. Enclose emotes in asterisks. Follow {conf.BOT_NAME}'s character.\n"
     if not history == []:
         # build prompt from history + query with
         # optional system message on first instruction
         for msg in history[conf.HISTORY_COUNT * -1:]:
-            prompt += "<|user|>" + cleanup_message(msg[0])
-            prompt += "<|model|>" + cleanup_message(msg[1]) + "\n"
+            prompt += f"<|user|>{cleanup_message(msg[0])}"
+            prompt += f"<|model|>{cleanup_message(msg[1])}\n"
 
-    prompt += "<|user|>" + query + "<|model|>"
+    prompt += f"<|user|>{query}<|model|>"
 
     prompt = prompt.replace("###USERNAME###", user_name)
 

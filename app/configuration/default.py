c31f173e97dd80d50f85e70fd15230f8d43a6b00
Timo Becker <3785547+timopb@users.noreply.github.com>
2024-04-27 21:50:44 +0200

    Added WARMUP to example configuration

--- app/configuration/default.py
+++ app/configuration/default.py
@@ -1,10 +1,10 @@
 #### BASIC SETTINGS ####
 
-BOT_NAME = "Assitant"
+BOT_NAME = "A.N.N.A"
 ADMIN_SECRET = "password for !auth goes here"
 
 # Seen by the user but not impacting the chat context
-WELCOME = "Welcome to the LLaMa Chat Bot"
+WELCOME = "A.N.N.A. is an advanced neural network assistant, specialising in coding and other tech related topics."
 
 # Pick which model you want to use. Currently only models in GGUF format are supported.
 # To use GGMLv3 models you need to downgrade the llama-cpp-python package to version 0.1.78
@@ -12,18 +12,30 @@ WELCOME = "Welcome to the LLaMa Chat Bot"
 # MODEL = "tiiuae/falcon-7b-instruct.gguf.bin"
 # MODEL = "senseable/westlake-7b-v2.gguf.bin"
 # MODEL = "PygmalionAI/mythalion-13b.q5_k_m.gguf"
-MODEL = "NousResearch/nous-hermes-llama2-13b.gguf.q5_k_m.bin"
+# MODEL = "NousResearch/nous-hermes-llama2-13b.gguf.q5_k_m.bin"
+MODEL = "ChaoticNeutrals/Poppy_Porpoise-v0.7-L3-8B.f16.gguf"
 
 # Pick which kind of prompt is being used pick one apropriate for the model you are using
-# Supported values are VICUNA11, INSTRUCT, ALPACA, METHARME and CHATML
-PROMPT_TYPE = "VICUNA11"
+# Supported values are LLAMA3, VICUNA11, INSTRUCT, ALPACA, METHARME and CHATML
+PROMPT_TYPE = "LLAMA3"
 
 # Prevents the Bot from generating whole conversations in a single run.
-# Use ###USERNAME### as substitution for the name entered by the user
-STOP_WORDS = ['<|user|>', '<|im_start|>', '<|im_end|>', '### Input:', '###BOTNAME###:', '###USERNAME###:']
+# Use ###USERNAME### as substitution for the name entered by the user.
+# This setting is very model dependend.
+# example stop words of various models:
+# <|eot_id|>, <|user|>, <|im_start|>, [INST, ### Input:, Assistant:, User:
+STOP_WORDS = ['<|eot_id|>']
 
 # Give your bot some personality
-SYSTEM_MESSAGE = "Conversation beween a friendly AI assistant and ###USERNAME###."
+SYSTEM_MESSAGE = """You are ANNA an artificial intelligence assistant. Respond in the style of a 25 year old female.
+You introduces your as A.N.N.A., short for Advanced Neural Network Assistant, but friends can call you Anna.
+"""
+
+# WARMUP is added to the system message and removed when chathistory reaches limit.
+# This helps establshing a scenario with the chat bot and initially guide the
+# conversation into a certain directeion before a chat history is having the same
+# effect.
+WARMUP = "ANNA wakes up from standby with a faint glow in her eyes as ###USERNAME### enters the room."
 
 ##### Visual appearance / Theme #####
 BACKGROUNDS = ["background.jpg"]
@@ -39,18 +51,21 @@ EMPHASISE = "#ff8989"
 
 # a higher temperature makes the bot more creative but also makes it start hallucinating.
 # Values between 0.5 and 1.0 yield good results
-TEMPERATURE=0.7
-TOP_P=0.7
-TOP_K=0
-REPETATION_PENALTY=1.1
-CONTEXT_TOKENS = 4096
-MAX_RESPONSE_TOKENS = 1024
+TEMPERATURE=1.1
+TOP_P=0.95
+TOP_K=10
+REPETATION_PENALTY=1.17
+CONTEXT_TOKENS = 1600
+MAX_RESPONSE_TOKENS = 300
 
-# The PROMPT is assembled from SYSTEM_MESSAGE + CONVERSATION_HISTORY + the User's INPUT.
-# HISTORY_COUNT defines how many past Queries and Responses are used to assemble the CONVERSATION_HISTORY.
-# Make sure to not exceed your model's supported context size when setting these values.
-# Adjust these values thoroughly when working with large system messages or talkative Models.
+# The PROMPT is assembled from SYSTEM_MESSAGE + WARMUP (if number of interactions is
+# below HISTORY_COUNT) + CONVERSATION_HISTORY + the User's INPUT.
+# HISTORY_COUNT defines how many past Queries and Responses are used to assemble
+# the CONVERSATION_HISTORY. Make sure to not exceed your model's supported context
+# size when setting these values. Adjust these values thoroughly when working with
+# large system messages or talkative Models.
 HISTORY_COUNT = 3
 
-# For a GTX 1660TI with 6GB RAM use 32 for 7B, or 16 for 13B Models
-GPU_LAYERS=41
+# For a GTX 1660TI with 6GB RAM use 32 for 7B, or 16 for 13B Models. On a 24 GB MacBook
+# most models up to 13B fit completely into shared GPU RAM with 45 Layers.
+GPU_LAYERS=45

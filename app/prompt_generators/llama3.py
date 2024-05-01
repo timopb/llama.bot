43a9a5897205bb639777e73591368a409f2847a6
Timo Becker <3785547+timopb@users.noreply.github.com>
2024-05-01 22:14:20 +0200

    Fixed llama3 prompt static names

--- app/prompt_generators/llama3.py
+++ app/prompt_generators/llama3.py
@@ -33,14 +33,14 @@ def build_llama3_prompt(conf, query,history,user_name):
         # build prompt from history + query with
         # optional system message on first instruction
         for msg in history[conf.HISTORY_COUNT * -1:]:
-            prompt += f"<|start_header_id|>user<|end_header_id|>\n\n"
+            prompt += f"<|start_header_id|>{user_name}<|end_header_id|>\n\n"
             prompt += f"{cleanup_message(msg[0])}<|eot_id|>\n"
-            prompt += f"<|start_header_id|>assistant<|end_header_id|>\n\n"
+            prompt += f"<|start_header_id|>{conf.BOT_NAME}<|end_header_id|>\n\n"
             prompt += f"{cleanup_message(msg[1])}<|eot_id|>\n"
 
-    prompt += f"<|start_header_id|>user<|end_header_id|>\n\n"
+    prompt += f"<|start_header_id|>{user_name}<|end_header_id|>\n\n"
     prompt += f"{query}<|eot_id|>\n"
-    prompt += f"<|start_header_id|>assistant<|end_header_id|>\n"
+    prompt += f"<|start_header_id|>{conf.BOT_NAME}<|end_header_id|>\n"
 
 
     prompt = prompt.replace("###USERNAME###", user_name)
